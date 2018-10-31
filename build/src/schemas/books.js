1758fd7cfb546c6a43ec276714dedad9ac4bcd7d
Carolina Bracamonte <carolinabmen@gmail.com>
2018-10-31 00:20:01 -0700

    build folder

---
+++ build/src/schemas/books.js
@@ -0,0 +1,49 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Schema = _mongoose2.default.Schema;
+
+var BooksSchema = new Schema({
+
+    'name': {
+        type: String,
+        require: true
+    },
+    'description': {
+        type: String
+    },
+    'year': {
+        type: Number
+    },
+    'author': {
+        type: String
+    },
+    'rank': {
+        type: [Number]
+    },
+    'ratings': {
+        type: Schema.Types.ObjectId,
+        ref: 'ratings'
+    },
+    'pages': {
+        type: String
+    },
+    'image': {
+        type: String
+    },
+    'category': {
+        type: Schema.Types.ObjectId,
+        ref: 'categories'
+    }
+}, { collection: 'books', timestamps: true });
+
+exports.default = _mongoose2.default.model('books', BooksSchema);
