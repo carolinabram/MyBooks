3e4c681ad67a18fa46a63ed0ca960d8c600225bc
Carolina Bracamonte <carolinabmen@gmail.com>
2018-10-30 22:45:06 -0700

    first commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () =>{
     console.log('Server WORKS on port ' + PORT);
 })
-mongoose.connect('mongodb://gabo16:gabo16@ds123173.mlab.com:23173/backend');
+mongoose.connect('mongodb://carolina:carolina1@ds243963.mlab.com:43963/libreria');
 const db = mongoose.connection;
 db.on('error', () => console.log("failed to connect to database"))
     .once('open', ()=> console.log("Connected to the data base ", PORT))
