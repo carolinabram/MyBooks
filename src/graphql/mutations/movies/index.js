b6ccef0e4676d5ee85ccbe9e4b279847d08f4d98
Carolina Bracamonte <carolinabmen@gmail.com>
2018-10-30 22:40:32 -0700

    first commit

---
+++ src/graphql/mutations/movies/index.js
@@ -0,0 +1,14 @@
+import addMovie from './add';
+import updateMovie from './update';
+import deleteMovie from './delete';
+import addRank from './addRank'
+
+
+
+export default {
+    addMovie,
+    updateMovie,
+    deleteMovie,
+    addRank
+
+}
---
+++ src/graphql/types/movies.js
@@ -0,0 +1,108 @@
+import * as GRAPHQL from 'graphql';
+
+import { CategoryType } from './categories';
+import { RatingType } from './ratings';
+
+import Category from '../../schemas/categories';
+import Rating from '../../schemas/ratings';
+
+export const MovieType = new GRAPHQL.GraphQLObjectType({
+    name: 'Movies',
+    description: 'Types of Movies',
+    fields : () => ({
+        _id:{
+            type: GRAPHQL.GraphQLNonNull(GRAPHQL.GraphQLID)
+        },
+        name : {
+            type : GRAPHQL.GraphQLString
+        },
+        year: {
+            type: GRAPHQL.GraphQLString
+        },
+        director: {
+            type: GRAPHQL.GraphQLString
+        },
+        rank: {
+            type: GRAPHQL.GraphQLList(GRAPHQL.GraphQLFloat)
+        },
+        plot : {
+            type: GRAPHQL.GraphQLString
+        },
+        cast: {
+            type: GRAPHQL.GraphQLString
+        },
+        length: {
+            type: GRAPHQL.GraphQLString
+        },
+        image: {
+            type: GRAPHQL.GraphQLString
+        },
+        url: {
+            type: GRAPHQL.GraphQLString
+        },
+        rating: {
+            type: RatingType,
+            resolve(movie){
+                const {rating} = movie
+                return Rating.findById(rating).exec()
+            }
+        },
+        category: {
+            type: CategoryType,
+            resolve(movie){
+                const {category} = movie
+                return Category.findById(category).exec()
+            }
+        }
+
+        
+    })
+})
+export const MovieInputType = new GRAPHQL.GraphQLInputObjectType({
+    name: 'AddMovies',
+    description: 'Types of Movies',
+    fields : () => ({
+        name : {
+            type : GRAPHQL.GraphQLString
+        },
+        year: {
+            type: GRAPHQL.GraphQLString
+        },
+        director: {
+            type: GRAPHQL.GraphQLString
+        },
+        plot : {
+            type: GRAPHQL.GraphQLString
+        },
+        cast: {
+            type: GRAPHQL.GraphQLString
+        },
+        length: {
+            type: GRAPHQL.GraphQLString
+        },
+        image: {
+            type: GRAPHQL.GraphQLString
+        },
+        url: {
+            type: GRAPHQL.GraphQLString
+        },
+        rating: {
+            type: GRAPHQL.GraphQLString
+        },
+        category: {
+            type: GRAPHQL.GraphQLString
+        }
+
+        
+    })
+})
+
+export const RankMovieType = new GRAPHQL.GraphQLInputObjectType({
+    name: 'addRank',
+    description: 'Add rank to Movie',
+    fields: () =>({
+        rank:{
+            type:GRAPHQL.GraphQLFloat
+        }
+    })
+})
