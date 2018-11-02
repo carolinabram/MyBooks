36702442417a562b860280fb2194c4b1a9cfee8f
Carolina Bracamonte <carolinabmen@gmail.com>
2018-11-01 21:52:06 -0700

    favorites

--- src/graphql/queries/books/myfavorites.js
+++ src/graphql/queries/books/myfavorites.js
@@ -7,17 +7,24 @@ import { FavoriteType } from '../../types/favorites';
 
 const queryMyFavorites = {
     type: new GRAPHQL.GraphQLList(FavoriteType),
+    resolve() {
+        const favorites = Favorite.find().exec()
+        if (!favorites) throw new Error("Error at fetching favorites");
+        return favorites
+    }
+   /* type: new GRAPHQL.GraphQLList(FavoriteType),
     args: {
         user: {
             name: 'user',
-            type: GRAPHQL.GraphQLNonNull(GRAPHQL.GraphQLID)
+            type: GRAPHQL.GraphQLNonNull(GRAPHQL.GraphQLObjectType)
         }
     },
     resolve(root, params) {
         const favorites = Favorite.find(params.args).exec()
+        console.log('favoritos'+favorites);
        // const favorites = Book.findById(params.id).exec()
         return favorites
-    }
+    }*/
 }
 
 export default queryMyFavorites;
